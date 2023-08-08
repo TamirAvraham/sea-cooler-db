e43efaaf40c17234cb0bf5b2bafa3242ec24dc65
Krab Dzen <krabdzen@gmail.com>
2023-08-08 18:28:24 +0300

    Added Errors File

---
+++ src/error.rs
@@ -0,0 +1,28 @@
+use std::fmt::Debug;
+
+#[derive(Debug,PartialEq, Eq, PartialOrd, Ord)]
+pub enum Error {
+    CantSeekToPage(usize),
+    CantGetNodesFileForWrite,
+    CantGetValuesFileForWrite,
+    CantSeekToValue(usize),
+    CantWritePage,
+    CantWriteValue,
+    CantReadValue,
+    CantGetNode(usize),
+    CantWriteNode(usize),
+    CantWriteCacheToDisk((usize,usize)),
+    CantGetValue
+}
+
+pub type InternalResult<T>=Result<T,Error>;
+
+
+
+
+pub fn map_err<E: std::fmt::Debug>(new_error:Error)->impl FnOnce(E) -> Error{
+    move |e|{
+        println!("error :{:?}",e);
+        new_error
+    }
+}
