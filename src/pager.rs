b5f05e94893901d69074864ca8f8d879875e3bf7
tamir avraham <tamir.avrah@gmail.com>
2023-08-08 19:00:02 +0300

    added a pager struct and added mods

--- src/pager.rs
+++ src/pager.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, fs::File};
+
 const PAGE_SIZE: usize = 1024 * 4;
 const SIZE_OF_USIZE: usize = size_of::<usize>();
 const EMPTY_PAGE: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
@@ -13,3 +15,22 @@ pub const NODE_PARENT_OFFSET: usize = NODE_TYPE_SIZE;
 pub const NODE_KEY_COUNT_SIZE: usize = SIZE_OF_USIZE;
 pub const NODE_KEY_COUNT_OFFSET: usize = NODE_PARENT_OFFSET + NODE_PARENT_SIZE;
 pub const HEADER_SIZE: usize = NODE_KEY_COUNT_OFFSET + NODE_KEY_COUNT_SIZE;
+
+type PagerFile=RefCell<File>
+
+struct Pager {
+    nodes_file:PagerFile,
+    values_file:PagerFile,
+    max_page_id:usize
+}
+
+
+
+impl Pager {
+    #[inline]
+    fn file_to_pager_file(file:File) -> PagerFile {
+        RefCell::new(file)
+    }
+
+    pub fn 
+}
