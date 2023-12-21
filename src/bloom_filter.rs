a1a2eb658afeb21b0fdbb00f67757702345a3151
tamir avraham <tamir.avrah@gmail.com>
2023-12-22 00:53:56 +0200

    documented pager.rs

--- src/bloom_filter.rs
+++ src/bloom_filter.rs
@@ -3,12 +3,11 @@ use std::f64::consts::LN_2;
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
-
 const N: f64 = 4.0 * 1024.0 * 1024.0;
 const FPR: f64 = 0.02;
-const FPR_LN:f64=-3.9120230054;//ln.98
-pub const  M:f64 = -(N * FPR_LN) / (LN_2 * LN_2);
-const  K:f64=(M/ N) * LN_2;
+const FPR_LN: f64 = -3.9120230054; //ln.98
+pub const M: f64 = -(N * FPR_LN) / (LN_2 * LN_2);
+const K: f64 = (M / N) * LN_2;
 
 pub struct BloomFilter {
     pub bit_array: Vec<bool>,
@@ -21,32 +20,49 @@ impl BloomFilter {
         }
     }
 
-    pub fn insert(&mut self,element:&str){
-        (0..(K as usize)).for_each(|i|{
+    /// #  Description
+    /// function inserts an element to the bloom filter.
+    /// # Arguments
+    ///
+    /// * `element`:  element to insert into the bloom filter.
+    ///
+    /// returns: ()
+
+    pub fn insert(&mut self, element: &str) {
+        (0..(K as usize)).for_each(|i| {
             let mut hasher = DefaultHasher::new();
             element.hash(&mut hasher);
-            self.bit_array[(hasher.finish()%((M as u64)+i as u64)) as usize]=true;
+            self.bit_array[(hasher.finish() % ((M as u64) + i as u64)) as usize] = true;
         })
     }
 
-    pub fn contains(&self,element:&str)->bool{
-        (0..(K as usize)).all(|i|{
+    /// # Description
+    /// function checks if an element is in the bloom filter.
+    ///
+    /// # Arguments
+    ///
+    /// * `element`: element to look up
+    ///
+    /// returns: element in bloom filter?
+
+    pub fn contains(&self, element: &str) -> bool {
+        (0..(K as usize)).all(|i| {
             let mut hasher = DefaultHasher::new();
             element.hash(&mut hasher);
-            self.bit_array[(hasher.finish()%((M as u64)+i as u64)) as usize]
+            self.bit_array[(hasher.finish() % ((M as u64) + i as u64)) as usize]
         })
     }
-    pub fn get_array(&self)->&Vec<bool>{
+    pub fn get_array(&self) -> &Vec<bool> {
         &self.bit_array
     }
 }
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::BloomFilter;
 
     #[test]
-    fn test_bloom_filter(){
-        let mut bf=BloomFilter::new();
+    fn test_bloom_filter() {
+        let mut bf = BloomFilter::new();
         bf.insert("yosi1");
         bf.insert("yosi2");
         bf.insert("yosi3");
@@ -55,8 +71,5 @@ mod tests{
         assert!(bf.contains("yosi2"));
         assert!(bf.contains("yosi3"));
         assert!(!bf.contains("beni"));
-
-
-
     }
 }
